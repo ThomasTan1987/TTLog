7122c4e7a437f62a4e5b547448fc9ea13f4d41f0
ThomasTan1987 <914964758@qq.com>
2016-10-15 21:37:20 +0800

    升级版本

--- Classes/TTLog.h
+++ Classes/TTLog.h
@@ -8,12 +8,10 @@
 
 #import <Foundation/Foundation.h>
 #define LOG(...) [[TTLog sharedLog] log:__VA_ARGS__]
-#define SHOW_LOG [[TTLog sharedLog] showLogView]
 @interface TTLog : NSObject
 @property(nonatomic,assign)BOOL enableLog;
+@property(nonatomic,copy)NSString *path;
+@property(nonatomic,assign)unsigned long long maxSize;
 + (TTLog*)sharedLog;
 - (void)log:(NSString *)fmt,...;
-- (void)showLogView;
-- (void)hideLogView;
-- (BOOL)isShowLog;
 @end
--- Classes/TTLog.m
+++ Classes/TTLog.m
@@ -9,9 +9,6 @@
 #import "TTLog.h"
 #import <UIKit/UIKit.h>
 @interface TTLog()
-@property (nonatomic, strong)NSMutableString *logText;
-@property (nonatomic, strong)UIView *logView;
-@property (nonatomic, strong)UITextView *textView;
 @end
 @implementation TTLog
 static TTLog *instance = nil;
@@ -27,72 +24,38 @@ static TTLog *instance = nil;
 {
     self = [super init];
     if (self) {
-        _logText = [[NSMutableString alloc] init];
+        self.maxSize = 1024 * 1024;
     }
     return self;
 }
-
+- (NSString *)path
+{
+    if (_path) {
+        NSArray *paths = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
+        _path = [[paths objectAtIndex:0] stringByAppendingPathComponent:@"vipa_debug.log"];
+    }
+    return _path;
+}
 - (void)log:(NSString *)fmt,...
 {
     if (self.enableLog) {
         va_list args;
         va_start(args, fmt);
         //写到文件
+        NSFileHandle *fileHandle = [NSFileHandle fileHandleForWritingAtPath:self.path];
+        unsigned long long fileSize = [fileHandle seekToEndOfFile];
+        if (fileSize > self.maxSize) {
+            [fileHandle truncateFileAtOffset:fileSize/2];
+        }
         NSDateFormatter *format = [[NSDateFormatter alloc] init];
         [format setDateFormat:@"MM-dd hh:mm:ss"];
         NSString *dateString = [format stringFromDate:[NSDate date]];
         NSString *logString = [[NSString alloc] initWithFormat:fmt arguments:args];
-        [self.logText appendFormat:@"[%@] %@\n",dateString,logString];
-        if (self.logView.superview) {
-            dispatch_async(dispatch_get_main_queue(), ^{
-                self.textView.text = self.logText;
-            });
-        }
+        
+        
+        [fileHandle writeData:[[NSString stringWithFormat:@"[%@] %@\n",dateString,logString] dataUsingEncoding:NSUTF8StringEncoding]];
         va_end(args);
     }
-}
-- (BOOL)isShowLog
-{
-    if (self.logView && self.logView.superview) {
-        return YES;
-    }else{
-        return NO;
-    }
-}
-- (void)showLogView
-{
-    UIWindow *window = [UIApplication sharedApplication].keyWindow;
-    if (!self.logView) {
-        self.logView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height)];
-        [window addSubview:self.logView];
-        
-        UITextView *textView = [[UITextView alloc] initWithFrame:self.logView.bounds];
-        self.textView = textView;
-        self.textView.textColor = [UIColor whiteColor];
-        self.textView.backgroundColor = [UIColor blackColor];
-        textView.text = self.logText;
-        [self.logView addSubview:textView];
-        textView.editable = NO;
-        
-        
-        UIButton *btnReset = [UIButton buttonWithType:UIButtonTypeCustom];
-        [btnReset setTitle:@"清除" forState:UIControlStateNormal];
-        btnReset.backgroundColor = [UIColor grayColor];
-        btnReset.frame = CGRectMake(0, self.logView.bounds.size.height - 30, 50, 30);
-        [self.logView addSubview:btnReset];
-        [btnReset addTarget:self action:@selector(reset) forControlEvents:UIControlEventTouchUpInside];
-    }
-    self.textView.text = self.logText;
-    [window addSubview:self.logView];
-}
-- (void)reset
-{
-    self.logText = [[NSMutableString alloc] init];
-    self.textView.text = @"";
-}
-- (void)hideLogView
-{
-    [self.logView removeFromSuperview];
 }
 #pragma private
 
